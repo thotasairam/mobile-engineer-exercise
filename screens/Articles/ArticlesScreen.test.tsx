abc2c6e3bcc0d372924b5e70ecd09a2ffd04f637
Sai Ram Thota <thotasairam@gmail.com>
2022-10-18 12:38:26 +0530

    Added testing library & @shopify/flashlist

---
+++ screens/Articles/ArticlesScreen.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react-native';
+import { ArticlesScreen } from './ArticlesScreen';
+
+describe('Articles Screen', () => {
+  it('should render correctly', () => {
+    render(<ArticlesScreen />);
+  });
+});
