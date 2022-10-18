6d9ab4163451025ab0d44d9149c949d2990bd26a
Sai Ram Thota <thotasairam@gmail.com>
2022-10-18 10:39:44 +0530

    Initial commit

---
+++ app.config.ts
@@ -0,0 +1,11 @@
+import { ExpoConfig } from '@expo/config-types';
+
+// In SDK 47 and higher, use the following import instead:
+// import { ExpoConfig } from 'expo/config';
+
+const config: ExpoConfig = {
+    name: 'fm-news',
+    slug: 'dev/@thotasairam/fm-news'
+};
+
+export default config;
